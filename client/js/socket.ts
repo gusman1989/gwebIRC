f96cffb5559de474906a14564c2ccf7998a1a1d2
gusmanpakaya1989 <gusmanpakaya1989@gmail.com>
2025-01-10 04:53:45 +0000

    modified:   client/js/socket.ts

--- client/js/socket.ts
+++ client/js/socket.ts
@@ -3,7 +3,7 @@ import type {ServerToClientEvents, ClientToServerEvents} from "../../shared/type
 
 type Socket = rawSocket<ServerToClientEvents, ClientToServerEvents>;
 
-const socket: Socket = io('http://141.95.28.213:5002', {
+const socket: Socket = io('https://141.95.28.213:5002', {
 	transports: JSON.parse(document.body.dataset.transports || "['polling', 'websocket']"),
 	path: window.location.pathname + "socket.io/",
 	autoConnect: false,
